4de0e5dc4da1198f372e7480a299e5e6a7463210
ZhangJinga <qq@.com>
2019-07-22 16:07:07 +0800

    Update wx.php

--- application/extra/wx.php
+++ application/extra/wx.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'app_id'=>'wxf1052566324f5d5c',
-    'app_secret'=>'50c8f470e8531c66c8b6949f5e1ecd14',
+    'app_id'=>'',
+    'app_secret'=>'',
     'login_url' => "https://api.weixin.qq.com/sns/jscode2session?" .
         "appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
-    'pay_back_url'=>'http://z.cn/api/v1/pay/notify',
+    'pay_back_url'=>'https://zerg.bug3.cn/api/v1/pay/notify',
 ];
